035f15e1043728c4ffa3b2bfad5fe438eb74a5a1
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-07 21:25:14 +0600

    lecture 01 - basic router setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+import Home from './Components/Home/Home';
+import Orders from './Components/Orders/Orders';
+import Main from './Layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>,
+        },
+
+        {
+          path: '/orders',
+          element: <Orders></Orders>,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/__steps.js
@@ -0,0 +1,11 @@
+/* 
+//* Basic Router Setup 
+1. npm i react-router-dom
+2. create a router using createBrowserRouter 
+3. add RouterProvider and set router prop
+4. Create a lot of Components
+5. Create Main layout with some common part and one Changing part based on the route
+6. set main layout at the root of the route
+7. set children routes
+8. set links inside the header component
+*/
